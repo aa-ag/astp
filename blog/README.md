f449b3e70ad78701f87acafadf86b0b673c7bc0f
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-14 10:21:03 -0600

    added Readme

---
+++ blog/README.md
@@ -0,0 +1,3 @@
+### Automated Testing Class
+
+Class `repo` [here](https://github.com/schoolofcode-me/testing-python-apps)
--- blog/tests/unit/test_post.py
+++ blog/tests/unit/test_post.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 from post import Post
 
+
 class PostTest(TestCase):
     def test_create_post(self):
-        p = Post('Test', 'Test Content')
+        p = Post('Test', 'Test content')
 
         self.assertEqual('Test', p.title)
-        self.assertEqual('Test Content', p.content)
-
+        self.assertEqual('Test content', p.content)
