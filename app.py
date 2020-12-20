65c9463d7479a71ec84568b6f86a2c65a3a8d357
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-20 17:50:08 -0600

    added test_print_blogs

---
+++ app.py
@@ -0,0 +1,11 @@
+# mapping of blog_name to Blog object
+blogs = dict()
+
+
+def menu():
+    pass
+
+
+def print_blogs():
+    for k, v in blogs.items():
+        print('- {}'.format(v))
---
+++ test_app.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import patch
+import app
+from blog import Blog
+
+
+class AppTest(unittest.TestCase):
+    def test_print_blogs(self):
+        blog = Blog('Test title', 'Test author')
+
+        app.blogs = {
+            'Test': blog
+        }
+
+        with unittest.mock.patch('builtins.print') as mocked_print:
+            app.print_blogs()
+            mocked_print.assert_called_with(
+                '- Test title by Test author (0 posts)')
+
+
+if __name__ == '__main__':
+    unittest.main()
