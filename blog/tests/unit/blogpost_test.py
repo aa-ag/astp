46261b4b0ffc84bf806f692d0e5d242afdcf4e57
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-23 23:28:51 -0600

    added docstring for first unit test

--- blog/tests/unit/blogpost_test.py
+++ blog/tests/unit/blogpost_test.py
@@ -4,6 +4,9 @@ from post import Post as post_class
 
 class PostTest(TestCase):
     def test_create_post(self):
+        '''
+         creates an object and compares that properties match
+        '''
         p = Post("Example title", "Lorem ipsum... (example content).")
         self.assertEqual("Example title", p.title)
         self.assertEqual("Lorem ipsum... (example content).", p.content)
