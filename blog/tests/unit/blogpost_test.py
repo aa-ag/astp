ebacd5b547e7160566ca5bdc9361bd14c57acfde
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-23 23:39:16 -0600

    refactored to pass 1/1 tests; config'ed env to execute tests

--- blog/tests/unit/blogpost_test.py
+++ blog/tests/unit/blogpost_test.py
@@ -1,13 +1,12 @@
 from unittest import TestCase
-from blog.post import Post
-from post import Post as post_class
+import post
+
 
 class PostTest(TestCase):
     def test_create_post(self):
         '''
          creates an object and compares that properties match
         '''
-        p = Post("Example title", "Lorem ipsum... (example content).")
+        p = post.Post("Example title", "Lorem ipsum... (example content).")
         self.assertEqual("Example title", p.title)
         self.assertEqual("Lorem ipsum... (example content).", p.content)
-
