5556675937badae2ece95cf82355d12f4d7c6c77
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-23 23:26:44 -0600

    added p variable for post sample with title and content as parameters using class Post

--- blog/tests/unit/blogpost_test.py
+++ blog/tests/unit/blogpost_test.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
+from blog.post import Post
 from post import Post as post_class
 
 class PostTest(TestCase):
     def test_create_post(self):
+        p = Post("Example title", "Lorem ipsum... (example content).")
         pass
 
