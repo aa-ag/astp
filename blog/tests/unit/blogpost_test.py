8d767e451af1984c3cc414a50562125be74b633e
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-24 18:45:38 -0600

    edited test_create_post to fail with wrong title

--- blog/tests/unit/blogpost_test.py
+++ blog/tests/unit/blogpost_test.py
@@ -8,5 +8,5 @@ class PostTest(TestCase):
          creates an object and compares that properties match
         '''
         p = post.Post("Example title", "Lorem ipsum... (example content).")
-        self.assertEqual("Example title", p.title)
+        self.assertEqual("testing wrong title", p.title)
         self.assertEqual("Lorem ipsum... (example content).", p.content)
