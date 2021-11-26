5c80ff853d90a97aa5b8ce9bae2b09f141384815
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-25 19:59:52 -0600

    added assertEqual title to check title creation for a blog

--- project/blog.py
+++ project/blog.py
@@ -2,4 +2,4 @@ class Blog:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.post = list()
+        self.posts = list()
--- project/tests/unit/blog_test.py
+++ project/tests/unit/blog_test.py
@@ -1,2 +1,8 @@
 from unittest import TestCase
 from blog import Blog
+
+class BlogTest(TestCase):
+    def test_blog_creation(self):
+        b = Blog("sample Title", "sample Author")
+        self.assertEqual("sample Title", b.title)
+        
