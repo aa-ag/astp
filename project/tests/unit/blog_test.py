e0f90bbf35ed5cc7fa83dee59b1c4aa0aee0e7ab
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-27 19:25:58 -0600

    fixed string in __repr__ method.  was missing a dot

--- project/tests/unit/blog_test.py
+++ project/tests/unit/blog_test.py
@@ -13,8 +13,8 @@ class BlogTest(TestCase):
     def test__repr__(self):
         b1 = Blog("Test","Test Author")
         b2 = Blog("Another Test", "Another Author")
-        self.assertEqual(b1.__repr__(), "Test by Test Author (0 posts)")
-        self.assertEqual(b2.__repr__(), "Another Test by Another Author (0 posts)")
+        self.assertEqual(b1.__repr__(), "Test by Test Author (0 posts).")
+        self.assertEqual(b2.__repr__(), "Another Test by Another Author (0 posts).")
 
     def create_post(self):
         pass
