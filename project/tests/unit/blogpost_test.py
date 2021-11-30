4f88524e9e44cfce8488500c9e16e748afca7d77
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-29 22:17:24 -0600

    refactored blogpost_test to include separators and docstrings

--- project/tests/unit/blogpost_test.py
+++ project/tests/unit/blogpost_test.py
@@ -1,7 +1,9 @@
+############------------ IMPORTS ------------############
 from unittest import TestCase
 import post
 
 
+############------------ CLASS(ES) ------------############
 class PostTest(TestCase):
     def test_create_post(self):
         '''
@@ -12,6 +14,13 @@ class PostTest(TestCase):
         self.assertEqual("Lorem ipsum... (example content).", p.content)
 
     def test_json_function(self):
+        '''
+         creates and object and compares with an expected json function
+         testing both creation, components & type
+        '''
         p = post.Post("Example title", "Lorem ipsum... (example content).")
         expected = {"title": "Example title", "content": "Lorem ipsum... (example content)."}
         self.assertDictEqual(expected, p.json_function())
+
+
+############------------ DRIVER CODE ------------############
