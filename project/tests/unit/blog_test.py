cec5cebb2554af7f0d2b4e911def9bc776e62ca6
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-30 19:40:37 -0600

    refactored file structure;  removed test_create_post_in_post() from blog_test.py in unit tests because it wasn't a unit test:  method was testing, (i) post creation, (ii) post lenght, (iii) title creation, & (iv) content creation.  Moved into integration/integration_blog_test.py

--- project/tests/unit/blog_test.py
+++ project/tests/unit/blog_test.py
@@ -38,11 +38,4 @@ class BlogTest(TestCase):
         }
         self.assertDictEqual(expected, b.json_function())
 
-    def test_create_post_in_post(self):
-        b = blog.Blog("sample Title", "sample Title", ["a blog post", "another blog post"])
-        b.create_post("Test Post", "Test Content")
-
-        self.assertEqual(len(b.posts), 1)
-        self.assertEqual(b.posts[0].title, "Test Post")
-        self.assertEqual(b.posts[0].content, "Test Content")
     
---
+++ project/tests/unit/integration_blog_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+import blog
+from project.blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_post_in_post(self):
+        b = blog.Blog("sample Title", "sample Title", ["a blog post", "another blog post"])
+        b.create_post("Test Post", "Test Content")
+
+        self.assertEqual(len(b.posts), 1)
+        self.assertEqual(b.posts[0].title, "Test Post")
+        self.assertEqual(b.posts[0].content, "Test Content")
+    
