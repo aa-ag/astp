4f581a01183698f3d6680c7303e71967a1693ce1
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-04 13:32:27 -0600

    test_print_blogs refactored, all tests passing

--- project/tests/system/app_test.py
+++ project/tests/system/app_test.py
@@ -2,6 +2,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 import app
+from project.blog import Blog
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 
@@ -9,9 +10,11 @@ import app
 ############------------ CLASS(ES) ------------############
 class AppTest(TestCase):
     def test_print_blogs(self):
+        b = Blog('Test', 'Test Author')
+        app.blogs = {'Test': b}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
-            mocked_print.assert_called_with('Test Blog')
+            mocked_print.assert_called_with('Test by Test Author (0 posts)')
 
 
 ############------------ DRIVER CODE ------------############
