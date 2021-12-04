5bda08d7f525ff485eaa20d5960f1e7f5f25701f
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-04 13:57:32 -0600

    added test_is_print_blogs_function_printed method in AppTest class to test if print_blogs is called from app.menu

--- project/tests/system/app_test.py
+++ project/tests/system/app_test.py
@@ -2,6 +2,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 import app
+from project.app import print_blogs
 from project.blog import Blog
 
 ############------------ GLOBAL VARIABLE(S) ------------############
@@ -9,6 +10,16 @@ from project.blog import Blog
 
 ############------------ CLASS(ES) ------------############
 class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+    
+    def test_is_print_blogs_function_printed(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called(print_blogs)
+    
     def test_print_blogs(self):
         b = Blog('Test', 'Test Author')
         app.blogs = {'Test': b}
@@ -17,10 +28,7 @@ class AppTest(TestCase):
             mocked_print.assert_called_with('Test by Test Author (0 posts)')
 
 
-    def test_menu_prints_prompt(self):
-        with patch('builtins.input') as mocked_input:
-            app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+    
 
 
 ############------------ DRIVER CODE ------------############
