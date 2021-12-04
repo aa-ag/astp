f69d1f4719db7d81773780e5effd0bdea669a45d
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-03 22:00:29 -0600

    added path import

--- project/tests/system/app_test.py
+++ project/tests/system/app_test.py
@@ -1,5 +1,6 @@
 ############------------ IMPORTS ------------############
 from unittest import TestCase
+from unittest.mock import patch
 import app
 
 ############------------ GLOBAL VARIABLE(S) ------------############
