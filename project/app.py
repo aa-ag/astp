f555e5820781a78f8cdea4db628590b3402618e1
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-04 13:39:37 -0600

    added selection logic in menu function

--- project/app.py
+++ project/app.py
@@ -9,6 +9,14 @@ blogs = dict()
 def menu():
     print_blogs()
 
+    selection = input(
+        'Press "c" to create a blog\n' +
+        '"l" to list all blogs\n' +
+        '"p" to create a post\n' +
+        '"q" to quit the program\n\n--> go ahead:  '
+    )
+
+    print(selection)
 
 def print_blogs():
     '''
