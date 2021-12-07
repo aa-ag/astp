b5833ab34fe43fa9ff1763bee700eabf8ce8553a
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-07 11:28:13 -0600

    added test_print_post and separated each method with an additional line break for readability

--- project/tests/system/app_test.py
+++ project/tests/system/app_test.py
@@ -15,18 +15,21 @@ class AppTest(TestCase):
     #         app.menu()
     #         mocked_input.assert_called_with(app.MENU_PROMPT)
     
+    
     def test_menu_calls_print_blogs(self):
         with patch('app.list_blogs') as mocked_print_blogs:
             with patch('builtins.input', return_value='q'):
                 app.menu()
                 mocked_print_blogs.assert_called()
     
+
     def test_print_blogs(self):
         b = Blog('Test', 'Test Author')
         app.blogs = {'Test': b}
         with patch('builtins.print') as mocked_print:
             app.list_blogs()
             mocked_print.assert_called_with("Test by Test Author (0 posts).")
+
 
     def test_ask_create_blog(self):
         with patch('builtins.input') as mocked_input:
@@ -35,6 +38,7 @@ class AppTest(TestCase):
 
             self.assertIsNotNone(app.blogs['This is a fake Title'])
 
+
     def test_ask_read_blog(self):
         b = Blog('Test', 'Test Author')
         app.blogs = {'Test': b}
@@ -42,6 +46,10 @@ class AppTest(TestCase):
             with patch('app.print_posts') as mocked_print_posts:
                 app.ask_read_blog()
                 mocked_print_posts.assert_called_with(b)
+
+
+    def test_print_post(self):
+        pass
 
 
 ############------------ DRIVER CODE ------------############
