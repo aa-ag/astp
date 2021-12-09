2e4a09ffd6b8312bbd9fa34957cae190e6913e08
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-09 15:11:44 -0600

    added home_page function to create home page message/view for our application

--- flask_endpoint_project/app.py
+++ flask_endpoint_project/app.py
@@ -5,6 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 ############------------ FUNCTION(S) ------------############
+@app.route('/')
+def home_page():
+    '''
+     this is the home page
+     of our application
+    '''
+    return {'message': 'Hello, Wolrd!'}
 
 
 ############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    app.run()
