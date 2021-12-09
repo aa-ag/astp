ca6a5a28136e5a6c5752c45ab228ed648d26df2b
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-09 15:46:44 -0600

    added logic to check that response is not a 500 or a 429 as an alternative

--- flask_endpoint_project/tests/system/home_test.py
+++ flask_endpoint_project/tests/system/home_test.py
@@ -13,11 +13,21 @@ class TestHome(TestCase):
             # endpoint works as expected
             response = c.get('/')
 
-            possible_http_responses = [200, 201]
+            # check that http response is ok
+            # success_http_responses = [200, 201]
 
-            self.assertIn(
-                response.status_code, 
-                possible_http_responses
+            # self.assertIn(
+            #     response.status_code, 
+            #     success_http_responses
+            # )
+
+            # alternatively, check that http response
+            # is not a particular number
+            fail_http_responses = [500, 429]
+
+            self.assertNotIn(
+                response.status_code,
+                fail_http_responses
             )
             
             expected = {'message': 'Hello, Wolrd!'}
