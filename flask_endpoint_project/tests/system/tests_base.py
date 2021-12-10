75834bcfd1e2a846c683958d8f81d17a0e8d5635
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-10 17:35:33 -0600

    added BaseTest class and its first method: setUp

--- flask_endpoint_project/tests/system/tests_base.py
+++ flask_endpoint_project/tests/system/tests_base.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 from app import app
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-
+class BaseTest(TestCase):
+    def setUp(self):
+        pass
 
 ############------------ FUNCTION(S) ------------############
 
