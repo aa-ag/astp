b04744e407db39e7ec40edcc8be1ea618f8e6331
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-13 20:54:41 -0600

    reverted back to *_test.py format for tests

--- rest_api_project/tests/unit/models/item_test.py
+++ rest_api_project/tests/unit/models/item_test.py
@@ -4,7 +4,15 @@ from models.item import ItemModel
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+class ModelTest(TestCase):
+    def test_model_name(self):
+        expected = 'items'
+        self.assertEqual(ItemModel.__tablename__, expected)
 
+
+    def test_test(self):
+        expected = "a"
+        self.assertEqual(expected, 'a')
 
 ############------------ FUNCTION(S) ------------############
 
