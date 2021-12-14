0ed288c7d0b5cbbcde1744fbb096924d987474ce
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-13 20:23:36 -0600

    edited settings.json to invert test naming convention;  also added test_item.py's base structure & imports

---
+++ rest_api_project/tests/unit/models/test_item.py
@@ -0,0 +1,12 @@
+############------------ IMPORTS ------------############
+from unittest import TestCase
+from models.item import ItemModel
+
+
+############------------ GLOBAL VARIABLE(S) ------------############
+
+
+############------------ FUNCTION(S) ------------############
+
+
+############------------ DRIVER CODE ------------############
