a3b4d0d3aea7edfd32505f361d632b3b96a7981f
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-14 18:01:46 -0600

    added test_input_types method to test the input types passed to ItemModel before a record is created

--- rest_api_project/tests/unit/models/item_test.py
+++ rest_api_project/tests/unit/models/item_test.py
@@ -9,8 +9,33 @@ class ModelTest(TestCase):
         expected = 'items'
         self.assertEqual(ItemModel.__tablename__, expected)
 
+    def test_input_types(self):
+        pass
 
-############------------ FUNCTION(S) ------------############
+    '''
+class ItemModel(db.Model):
+    __tablename__ = 'items'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
 
-############------------ DRIVER CODE ------------############
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def json(self):
+        return {'name': self.name, 'price': self.price}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+    '''
