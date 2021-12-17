1db3a61c0f7ebd6e175d2781cddf1712dfbbe4b3
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-16 22:42:25 -0600

    added imports

---
+++ rest_api_project/tests/base_test.py
@@ -0,0 +1,13 @@
+############------------ IMPORTS ------------############
+from unittest import TestCase
+from app import app
+from db import db
+
+
+############------------ GLOBAL VARIABLE(S) ------------############
+
+
+############------------ FUNCTION(S) ------------############
+
+
+############------------ DRIVER CODE ------------############
