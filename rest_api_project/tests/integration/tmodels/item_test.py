bee89a8868ae90dbb8bc8378329d0a89a9337f5e
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-17 15:45:28 -0600

    added base structure and imports to item_test.py in integration tests

---
+++ rest_api_project/tests/integration/tmodels/item_test.py
@@ -0,0 +1,7 @@
+############------------ IMPORTS ------------############
+from tests.base_test import BaseTets
+from models.item import ItemModel
+
+
+############------------ TEST(S) ------------############
+
