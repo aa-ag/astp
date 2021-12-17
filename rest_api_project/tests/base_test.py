e3066ca460f957b3eaa19d90c623f9a37211e5dd
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-17 14:50:07 -0600

    added app.config variable to create new db for testing purposes

--- rest_api_project/tests/base_test.py
+++ rest_api_project/tests/base_test.py
@@ -7,6 +7,9 @@ from db import db
 ############------------ CLASS(ES) ------------############
 class BaseTets(TestCase):
     def setUp(self):
+        # to ensure a db exists,
+        # it setUp creates one
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
         pass
 
     def tearDown(self):
