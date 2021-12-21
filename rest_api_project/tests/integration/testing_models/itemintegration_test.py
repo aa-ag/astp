a4de075428ad5b958724fa0adee77cd6142d4467
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-20 19:53:14 -0600

    added notes of documentation just before each of the steps in the test_crud method

--- rest_api_project/tests/integration/testing_models/itemintegration_test.py
+++ rest_api_project/tests/integration/testing_models/itemintegration_test.py
@@ -6,11 +6,16 @@ from models.item import ItemModel
 ############------------ TEST(S) ------------############
 class ItemTest(BaseTest):
     def test_crud(self):
+        # generate context / all reqs
         with self.app_context():
+            # create a test item in doc/db
             item = ItemModel('Test', 9.99)
 
+            # make sure it doesn't already exist
             self.assertIsNone(ItemModel.find_by_name('Test'))
 
+            # save it to the db if it doesn't
             item.save_to_db()
 
+            # assert that it does exist in the db after creating it
             self.assertIsNotNone(ItemModel.find_by_name('Test'))
