7367559b4badc136a7b52090cb5bd95151143c4f
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-20 19:52:39 -0600

    added assertIsNone to make sure that test item being created doesn't already exist in the db

--- rest_api_project/tests/integration/testing_models/itemintegration_test.py
+++ rest_api_project/tests/integration/testing_models/itemintegration_test.py
@@ -8,5 +8,9 @@ class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
             item = ItemModel('Test', 9.99)
+
+            self.assertIsNone(ItemModel.find_by_name('Test'))
+
             item.save_to_db()
+
             self.assertIsNotNone(ItemModel.find_by_name('Test'))
