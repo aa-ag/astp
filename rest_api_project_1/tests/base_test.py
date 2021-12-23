457e99532c693940c5a4d40d8ff20894d8c93527
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-22 21:58:56 -0600

    renamed rest_api_project to rest_api_project_1 to accomodate for second section of rest API testing

---
+++ rest_api_project_1/tests/base_test.py
@@ -0,0 +1,29 @@
+############------------ IMPORTS ------------############
+from unittest import TestCase
+from application import app
+from database import db
+
+
+############------------ CLASS(ES) ------------############
+class BaseTest(TestCase):
+    def setUp(self):
+        # to ensure a db exists,
+        # it setUp creates one
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        with app.app_context():
+            # initalize app
+            db.init_app(app)
+            # create all tables
+            db.create_all()
+
+        # get test client
+        self.app = app.test_client()
+        self.app_context = app.app_context
+
+    def tearDown(self):
+        # reset everything in db
+        with app.app_context():
+            # delete everythig from current session
+            db.session.remove()
+            # delete everything from tables in db
+            db.drop_all()
---
+++ rest_api_project_1/tests/unit/tmodels/item_test.py
@@ -0,0 +1,81 @@
+############------------ IMPORTS ------------############
+from models.item import ItemModel
+from tests.base_test import BaseTest
+
+
+############------------ GLOBAL VARIABLE(S) ------------############
+class ModelTest(BaseTest):
+    def test_create_item(self):
+        '''
+         tests item creation by attempting to create an item,
+         and let's tester know if there are discrepancies between
+         inputs & created item
+        '''
+        test_item = ItemModel('Test Item', 9.99)
+        self.assertEqual(
+            test_item.name, 
+            'Test Item',
+            'Item\'s name not equal to constructor argument.'
+        )
+        self.assertEqual(
+            test_item.price, 
+            9.99,
+            'Item\'s price not equal to constructor argument.'
+        )
+
+
+    def test_model_name(self):
+        '''
+         tests that ItemModel's table name is what's expected
+        '''
+        expected = 'items'
+        self.assertEqual(ItemModel.__tablename__, expected)
+
+
+    def test_input_types(self):
+        '''
+         ItemModel takes two inputs: name & price.
+         this test tests that ItemModel's inputs are in the
+         expected type: string for name and float for price
+        '''
+        dummy_item = ItemModel('Test Item', 1.00)
+        self.assertEqual(type(dummy_item.name), str)
+        self.assertEqual(type(dummy_item.price), float)
+
+
+    def test_price_decimals(self):
+        '''
+         ItemModel's price input expects a floating point value with two places
+        '''
+        dummy_item = ItemModel('Test Item', 1.33)
+        expected_precision = round(dummy_item.price, 2)
+        self.assertEqual(dummy_item.price, expected_precision)
+
+
+    def test_generate_json(self):
+        '''
+         tests ItemModel's generate_json method
+        '''
+        dummy_item = ItemModel('Test Item', 1.99)
+        expected_json = {'name': 'Test Item', 'price': 1.99}
+        dummy_json = dummy_item.generate_json()
+        self.assertEqual(
+            dummy_json, 
+            expected_json,
+            f"Json export of item is incorrect. Received {dummy_item.generate_json()}, expected {expected_json}"
+        )
+
+
+'''
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+'''
