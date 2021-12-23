ff22e5e155f5d889cb6f034111e94f63e1d779e7
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-22 22:10:48 -0600

    added note on test_crud to do next

--- rest_api_project_2/tests/integration/models/item_test.py
+++ rest_api_project_2/tests/integration/models/item_test.py
@@ -5,6 +5,8 @@ from tests.base_test import BaseTest
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
+            # this works in sqlite but wouldn't in postgres
+            # as the stores aren't yet created
             item = ItemModel('test', 19.99, 1)
 
             self.assertIsNone(ItemModel.find_by_name('test'),
