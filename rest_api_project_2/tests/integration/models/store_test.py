940a1385ef4624c0ce21f7012083930066a0a955
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-23 20:13:52 -0600

    added logic to check that by creating an empty store, the store is infact created and no items were added

--- rest_api_project_2/tests/integration/models/store_test.py
+++ rest_api_project_2/tests/integration/models/store_test.py
@@ -1,3 +1,7 @@
 from models.store import StoreModel
 from tests.base_test import BaseTest
 
+class StoreTest(BaseTest):
+    def test_create_store_items_empty(self):
+        store = StoreModel('test')
+        self.assertListEqual(store.items.all(), [])
