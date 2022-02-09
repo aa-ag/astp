1f108b3706b06d9a7314be03cf897f8f0616894a
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-09 17:03:33 -0600

    Add @given-decorated step_implementation function

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -4,3 +4,8 @@ from selenium import webdriver
 # reads from navigation feature
 use_step_matcher('re')
 
+
+@given('I am on the homepage')
+def step_implementation(context):
+    browser = webdriver.Chrome()
+    browser.get('http://127.0.0.1:5000')
