5ab32a02958f75a82e63b833389d219213b06f17
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-08 21:15:51 -0600

    Add add_post route

--- app.py
+++ app.py
@@ -18,6 +18,23 @@ def homepage():
 def blog_page():
     return render_template('blog.html', posts=posts)
 
+
+@app.route('/post', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        global posts
+
+        posts.append({
+            'title': title,
+            'content': content
+        })
+
+        return redirect(url_for('blog_page'))
+    return render_template('new_post.html')
+
+    
 ############------------ DRIVER CODE ------------############
 if __name__ == '__main__':
     app.run()
